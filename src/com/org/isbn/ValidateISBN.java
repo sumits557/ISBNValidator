be51aaf23ad402f330703a5656002d74b411dd63
sumits557 <sumits557@gmail.com>
2020-03-29 22:28:57 -0400

    Update with ISBN functionality for ISBN Validator.

--- src/com/org/isbn/ValidateISBN.java
+++ src/com/org/isbn/ValidateISBN.java
@@ -2,7 +2,23 @@ package com.org.isbn;
 
 public class ValidateISBN {
 
-    public static void main(String[] args) {
-	System.out.println("######(Inside main)######");
+    public boolean checkISBN(String isbn) {
+
+        if (isbn.length() != 10) {
+            throw new NumberFormatException(("ISBN number must be a 10 digit"));
+        }
+
+        int total = 0;
+        for(int i = 0; i < 10; i++) {
+            if (!Character.isDigit(isbn.charAt(i))) {
+                throw new NumberFormatException("ISBN numbers can only contain numberic digits");
+            }
+            total += isbn.charAt(i) * (10 - i);
+        }
+        if (total % 11 == 0) {
+            return  true;
+        } else {
+            return  false;
+        }
     }
 }
---
+++ test/ValidateISBNTest.java
@@ -0,0 +1,35 @@
+import com.org.isbn.ValidateISBN;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidateISBNTest {
+
+    @Test
+    public void checkAValidISBNTest(){
+        ValidateISBN  validateISBN = new ValidateISBN();
+        boolean result = validateISBN.checkISBN("0140449116");
+        assertTrue("first value", result);
+        result  = validateISBN.checkISBN("0140177396");
+        assertTrue("second value", result);
+    }
+
+    @Test
+    public void checkAInValidISBNTest(){
+        ValidateISBN  validateISBN = new ValidateISBN();
+        boolean result = validateISBN.checkISBN("0114044917");
+        assertFalse(result);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void nineDigitISBNNotAllowed() {
+        ValidateISBN  validateISBN = new ValidateISBN();
+        validateISBN.checkISBN("011404491");
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void nonNumericISBNAreNotAllowed() {
+        ValidateISBN  validateISBN = new ValidateISBN();
+        validateISBN.checkISBN("helloWorld");
+    }
+}
